c838e05b5d8007d2e88970c0724b998fbac35f94
Nadinnnka <tymoshyk02@gmail.com>
2024-07-25 19:55:06 +0300

    make build

---
+++ index.d85bac20.js
@@ -0,0 +1,2 @@
+!function e(n,t){if(Object.keys(t).length){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,e(l,t[a]),r.appendChild(l)}n.appendChild(r)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.d85bac20.js.map
